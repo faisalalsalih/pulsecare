9864ec6b2c7dd864a687e0bd56d7885228f9fc77
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-06-03 22:09:12 +0500

    Add the Physician Selector

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -17,6 +17,9 @@ import { createUser } from "@/lib/actions/patient.actions"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
 import { GenderOptions } from "@/contants"
 import { Label } from "../ui/label"
+import { Doctors } from "@/contants"
+import { SelectItem } from "@radix-ui/react-select"
+import Image from "next/image"
 
 
 
@@ -32,7 +35,7 @@ export enum FormFieldType {
 }
 
 
-const RegisterForm = ({ user } : { user: User }) => {
+const RegisterForm = ({ user }: { user: User }) => {
 
   const router = useRouter();
 
@@ -49,7 +52,7 @@ const RegisterForm = ({ user } : { user: User }) => {
   })
 
 
-  async function onSubmit({ name, email, phone } : z.infer<typeof UserFormValidation>){
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
 
     setIsLoading(true);
 
@@ -60,11 +63,11 @@ const RegisterForm = ({ user } : { user: User }) => {
 
       const user = await createUser(userData);
 
-      if(user) router.push(`/patients/${user.$id}/register`)
-      
-      
+      if (user) router.push(`/patients/${user.$id}/register`)
+
+
     }
-    catch(error) {
+    catch (error) {
       console.log(error);
     }
 
@@ -76,109 +79,193 @@ const RegisterForm = ({ user } : { user: User }) => {
 
   return (
     <>
-    <Form {...form}>
+      <Form {...form}>
 
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
 
-        <section className="space-y-4">
-          <h1 className="header">Welcome 👋</h1>
-          <p className="text-dark-700">Let us know more about yourself.</p>
-        </section>
+          <section className="space-y-4">
+            <h1 className="header">Welcome 👋</h1>
+            <p className="text-dark-700">Let us know more about yourself.</p>
+          </section>
 
 
-        <section className="space-y-6">
-          
-          <div className="mb-9 space-y-1">
-            <h2 className="sub-header">Personal Information</h2>
+          <section className="space-y-6">
+
+            <div className="mb-9 space-y-1">
+              <h2 className="sub-header">Personal Information</h2>
+            </div>
+
+          </section>
+
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.INPUT}
+            name="name"
+            label="Full name"
+            placeholder="Full name..."
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user" />
+
+          <div className="flex flex-col xl:flex-row gap-6">
+
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.INPUT}
+              name="email"
+              label="Email Address"
+              placeholder="Email address ..."
+              iconSrc="/assets/icons/email.svg"
+              iconAlt="email" />
+
+
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.PHONE_INPUT}
+              name="phone"
+              label="Phone Number"
+              placeholder="(5555) 123-4567"
+            />
+
+
           </div>
 
-        </section>
 
-        <CustomFormField
-        control={form.control}
-        fieldType={FormFieldType.INPUT}
-        name="name"
-        label="Full name"
-        placeholder="Full name..."
-        iconSrc="/assets/icons/user.svg"
-        iconAlt="user"/>
-
-        <div className="flex flex-col xl:flex-row gap-6">
-
-        <CustomFormField
-        control={form.control}
-        fieldType={FormFieldType.INPUT}
-        name="email"
-        label="Email Address"
-        placeholder="Email address ..."
-        iconSrc="/assets/icons/email.svg"
-        iconAlt="email"/>
+          <div className="flex flex-col gap-6 xl:flex-row">
 
 
-        <CustomFormField
-        control={form.control}
-        fieldType={FormFieldType.PHONE_INPUT}
-        name="phone"
-        label="Phone Number"
-        placeholder="(5555) 123-4567"
-        />
+            {/* DatePicker for Date of Birth */}
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.DATE_PICKER}
+              name="birthDate"
+              label="Date of Birth"
+              placeholder="DOB ..." />
+
+            {/* Radio Group  */}
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.SKELETON}
+              name="gender"
+              label="Gender"
+              renderSkeleton={(field) => (
+                <FormControl>
+
+                  <RadioGroup className="flex h-11 gap-6 xl:justify-between"
+                    onValueChange={field.onChange} defaultValue={field.value}>
+                    {
+                      GenderOptions.map((option) => (
+                        <div key={option} className="radio-group">
+
+                          <RadioGroupItem value={option} id={option} />
+
+                          <Label htmlFor={option} className="cursor-pointer">
+                            {option}
+                          </Label>
+
+                        </div>
+                      ))
+                    }
+                  </RadioGroup>
+
+                </FormControl>
+              )}
+            />
+
+          </div>
 
 
-        </div>
+          <div className="flex items-col gap-6 xl:flex-row">
+
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.INPUT}
+              name="address"
+              label="Address ..."
+              placeholder="14th Street, New York"
+            />
 
 
-      <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.INPUT}
+              name="occupation"
+              label="Occupation"
+              placeholder="Software Engineer"
+            />
 
 
-        {/* DatePicker for Date of Birth */}
-        <CustomFormField
-        control={form.control}
-        fieldType={FormFieldType.DATE_PICKER}
-        name="birthDate"
-        label="Date of Birth"
-        placeholder="DOB ..."/>
-
-        {/* Radio Group  */}
-        <CustomFormField
-        control={form.control}
-        fieldType={FormFieldType.SKELETON}
-        name="gender"
-        label="Gender"
-        renderSkeleton={(field) => (
-          <FormControl>
-
-            <RadioGroup className="flex h-11 gap-6 xl:justify-between"
-            onValueChange={field.onChange} defaultValue={field.value}>
-              {
-                GenderOptions.map((option) => (
-                  <div key={option} className="radio-group">
-
-                    <RadioGroupItem value={option} id={option} />
-
-                      <Label htmlFor={option} className="cursor-pointer">
-                        {option}
-                      </Label>
-                      
-                    
-                  </div>
-                ))
-              }
-            </RadioGroup>
-
-          </FormControl>
-        )}
-        />
-
-      </div>
+          </div>
 
 
-        <SubmitButton isLoading={isLoading}>
-          Get Started
-        </SubmitButton>
+          <div className="flex flex-col xl:flex-row gap-6">
+
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.INPUT}
+              name="emergencyContactName"
+              label="Emergency Contact name"
+              placeholder="Guardian's name"
+            />
 
 
-      </form>
-    </Form>
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.PHONE_INPUT}
+              name="emergencyContactNumber"
+              label="Emergency Contact Number"
+              placeholder="(5555) 123-4567"
+            />
+
+
+          </div>
+
+
+
+          <section className="space-y-6">
+
+            <div className="mb-9 space-y-1">
+              <h2 className="sub-header">Medical Information</h2>
+            </div>
+
+          </section>
+
+
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.SELECT}
+            name="primaryPhysician"
+            label="Primary Physician"
+            placeholder="Select a physician"
+          >
+            {Doctors.map((doctor) => (
+
+              <SelectItem key={doctor.name} value={doctor.name}>
+
+                <div className="flex items-center cursor-pointer gap-2">
+
+                  <Image
+                  src={doctor.image}
+                  width={32}
+                  height={32}
+                  alt={doctor.name}
+                  className="rounded-full border border-dark-500" />
+
+                  <p>{doctor.name}</p>
+                  
+                </div>
+
+              </SelectItem>
+            ))}
+          </CustomFormField>
+
+
+          <SubmitButton isLoading={isLoading}>
+            Get Started
+          </SubmitButton>
+
+
+        </form>
+      </Form>
     </>
   )
 }
--- components/shared/CustomFormField.tsx
+++ components/shared/CustomFormField.tsx
@@ -9,6 +9,7 @@ import PhoneInput from 'react-phone-number-input'
 import { E164Number } from "libphonenumber-js/core";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from '../ui/select'
 
 
 
@@ -107,6 +108,34 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           </FormControl>
 
         </div>
+      )
+
+    case FormFieldType.SELECT:
+      return (
+        <FormControl>
+
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+
+            <FormControl>
+
+              <SelectTrigger className="shad-select-trigger">
+
+                <SelectValue placeholder={placeholder} />
+
+              </SelectTrigger>
+
+
+            </FormControl>
+
+
+            <SelectContent className="shad-select-content">
+
+              {props.children}
+
+            </SelectContent>
+
+          </Select>
+        </FormControl>
       )
 
     case FormFieldType.SKELETON:
