0f663459c75eb5454b97e70ea14723d049aedfe9
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-06-22 18:57:04 +0500

    Add dialog and AppointmentModal

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -5,6 +5,7 @@ import RegisterForm from "@/components/forms/RegisterForm";
 import { getPatient, getUser } from "@/lib/actions/patient.actions";
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
+  
   const user = await getUser(userId);
   const patient = await getPatient(userId);
 
